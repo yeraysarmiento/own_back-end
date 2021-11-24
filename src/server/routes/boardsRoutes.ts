c27613c42939f11af597ec16b5a2fac0f52a5704
ysarmiento <yeray.sarmiento@hotmail.com>
2021-11-24 19:49:42 +0100

    Add createBoard controller

---
+++ src/server/routes/boardsRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import createBoard from "../controllers/boardsControllers";
+import boardsPath from "../paths/boardsPath";
+
+const router = express.Router();
+
+router.post(boardsPath.createBoard, createBoard);
+// router.delete(boardsPath.deleteBoard, deleteBoard);
+
+// router.patch(boardsPath.modifyBoard, updateBoard)
+
+export default router;
--- src/server/utils/RequestAuth.ts
+++ src/server/utils/RequestAuth.ts
@@ -3,6 +3,7 @@ import { Request } from "express";
 interface RequestAuth extends Request {
   params: any;
   userId?: string | undefined;
+  username?: string | undefined;
 }
 
 export default RequestAuth;
