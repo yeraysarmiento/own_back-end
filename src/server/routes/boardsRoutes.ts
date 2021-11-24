438d28c30ffbf3c1bf1f2b11ae53f7de1d2b7933
ysarmiento <yeray.sarmiento@hotmail.com>
2021-11-24 23:27:36 +0100

    Add modularisation of upload middleware

--- src/server/routes/boardsRoutes.ts
+++ src/server/routes/boardsRoutes.ts
@@ -1,28 +1,10 @@
 import express from "express";
-import multer from "multer";
-import path from "path";
 import { createBoard, deleteBoard } from "../controllers/boardsControllers";
 import firebase from "../middlewares/firebase";
 import boardsPath from "../paths/boardsPath";
+import upload from "../middlewares/upload";
 
 const router = express.Router();
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: "images",
-    filename: (req, file, callback) => {
-      const oldFilename = file.originalname;
-      const oldFilenameExtension = path.extname(oldFilename);
-      const oldFilenameWithoutExtension = oldFilename.replace(
-        oldFilenameExtension,
-        ""
-      );
-
-      const newFilename = `${oldFilenameWithoutExtension}-${Date.now()}-${oldFilenameExtension}`;
-      callback(null, newFilename);
-    },
-  }),
-});
 
 router.post(
   boardsPath.createBoard,
