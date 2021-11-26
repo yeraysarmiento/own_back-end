c8f5c76ebffed35233786e57ed79183b3c9a5d13
ysarmiento <yeray.sarmiento@hotmail.com>
2021-11-26 11:51:50 +0100

    Deleted unused variables

--- src/server/routes/papersRoutes.ts
+++ src/server/routes/papersRoutes.ts
@@ -1,12 +1,11 @@
-import express from "express";
-import createPaper from "../controllers/papersController";
-import authentication from "../middlewares/authentication";
-import papersPath from "../paths/papersPath";
+// import express from "express";
+// import authentication from "../middlewares/authentication";
+// import papersPath from "../paths/papersPath";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post(papersPath.createPaper, authentication, createPaper);
-// router.delete(papersPath.deletePaper, authentication, deletePaper);
-// router.patch(papersPath.createPaper, authentication, updatePaper);
+// router.post(papersPath.createPaper, authentication, createPaper);
+// // router.delete(papersPath.deletePaper, authentication, deletePaper);
+// // router.patch(papersPath.createPaper, authentication, updatePaper);
 
-export default router;
+// export default router;
